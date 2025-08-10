6197b31f0cebd03b90fe68b9509fa2dd2074fddb
semlimtes7 <joaofordfiesta433@gmail.com>
2025-08-10 07:03:55 -0300

    cardápio.js

---
+++ src/cardápio.js
@@ -0,0 +1,16 @@
+const cardapio = {
+  1: {
+    descricao: 'X-Burguer',
+    preco: 9.9,
+  },
+  2: {
+    descricao: 'X-Bacon',
+    preco: 10.0,
+  },
+  3: {
+    descricao: 'X-Tudo',
+    preco: 12.99,
+  },
+};
+
+exports.menu = cardapio;
